286641301130c7adaf75cb26a3b46a0c97d58369
reyesdavid97 <38224412+reyesdavid97@users.noreply.github.com>
2018-09-24 19:39:28 -0400

    Add files via upload

---
+++ books/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from books import views
+
+
+urlpatterns = [
+
+]
--- bookshop/urls.py
+++ bookshop/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# Use include() to add paths
+urlpatterns += [
+    path('books/', include('books.urls')),
+]
+
+# Use static() to add url mapping to serve static files during development (only)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
