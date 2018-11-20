6d1e9e76dc387c32e75909bc3d24ba393430752d
reyesdavid97 <38224412+reyesdavid97@users.noreply.github.com>
2018-11-20 17:03:49 -0500

    Add files via upload

--- purchases/models.py
+++ purchases/models.py
@@ -1,3 +1,13 @@
+from __future__ import unicode_literals
 from django.db import models
 
+from books.models import Book
+from django.contrib.auth import settings
 # Create your models here.
+
+class Purchase(models.Model):
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+  book = models.ForeignKey(Book, on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now_add=True)
+  def __str__(self):
+    return "User: " + self.user.username + ", Book: " + self.book.title
