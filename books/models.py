eab0844a783a807e7b5819e55733401ea98082da
reyesdavid97 <38224412+reyesdavid97@users.noreply.github.com>
2018-11-20 17:00:09 -0500

    Add files via upload

--- books/models.py
+++ books/models.py
@@ -1,108 +1,52 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.db import models
-from django.urls import reverse  # Used to generate URLs by reversing the URL patterns
-import uuid  # Required for unique book instances
-
-
-# Create your models here.
-
-
-class Genre(models.Model):
-    name = models.CharField(max_length=50, help_text='Enter a book genre (e.g. Science Fiction)')
-
-    def __str__(self):
-        # String for representing the Model object.
-        return self.name
-
-
-class Language(models.Model):
-    # Model representing a Language (e.g. English, French, Japanese, etc.)
-    name = models.CharField(max_length=50, help_text="Enter the book's language (e.g. English, French, Japanese, etc.)")
-
-    def __str__(self):
-        # String for representing the Model object.
-        return self.name
+from django.contrib.auth import settings
 
 
 class Author(models.Model):
-    # Model representing an author.
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField('Died', null=True, blank=True)
-
-    class Meta:
-        ordering = ['last_name', 'first_name']
-
-    def get_absolute_url(self):
-        # Returns the url to access a particular author instance.
-        return reverse('author-detail', args=[str(self.id)])
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
 
     def __str__(self):
-        # String for representing the Model object.
-        return f'{self.last_name}, {self.first_name}'
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
 
 
 class Book(models.Model):
-    # Model representing a book (but not a specific copy of a book).
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
-
-    # Foreign Key used because book can only have one author, but authors can have multiple books
-    # Author as a string rather than object because it hasn't been declared yet in the file.
-    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
-    isbn = models.CharField('ISBN', max_length=13, help_text='13 character ISBN code')
-
-    # ManyToManyField used because genre can contain many books. Books can cover many genres.
-    # Genre class has already been defined so we can specify the object above.
-    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
-    language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True)
-
-    rating = models.DecimalField(max_digits=3, decimal_places=2)
+    title = models.CharField(max_length=100)
+    author = models.ManyToManyField(Author)
+    cover_url = models.CharField(max_length=200)
+    description = models.TextField()
+    genre = models.ManyToManyField(Genre)
+    price = models.FloatField()
+    publisher = models.CharField(max_length=100)
+    release_date = models.DateTimeField('publishing date')
 
     def __str__(self):
-        # String for representing the Model object.
         return self.title
 
-    def get_absolute_url(self):
-        # Returns the url to access a detail record for this book.
-        return reverse('book-detail', args=[str(self.id)])
-
-    def display_genre(self):
-        # Create a string for the Genre.
-        return ', '.join(genre.name for genre in self.genre.all()[:3])
-
-    def display_rating(self):
-        # Create a string for the Rating.
-        return 'Rating: ' + rating
-
-    display_genre.short_description = 'Genre'
-
-
-class BookInstance(models.Model):
-    # Model representing a specific copy of a book (i.e. that can be borrowed from the library).
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book.')
-    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
-    imprint = models.CharField(max_length=200)
-    due_back = models.DateField(null=True, blank=True)
-
-    LOAN_STATUS = (
-        ('m', 'Maintenance'),
-        ('o', 'On loan'),
-        ('a', 'Available'),
-        ('r', 'Reserved'),
-    )
-
-    status = models.CharField(
-        max_length=1,
-        choices=LOAN_STATUS,
-        blank=True,
-        default='m',
-        help_text='Book availability',
-    )
-
     class Meta:
-        ordering = ['due_back']
+        ordering = ['title']
+
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True,on_delete=models.DO_NOTHING)
+    rating = models.PositiveIntegerField(blank=True, null=True)
+    comments = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    anonymous = models.BooleanField(default=False)
 
     def __str__(self):
-        # String for representing the Model object.
-        return f'{self.id} ({self.book.title})'
+        return 'review for {0}: rating {1}, comment {2}, anonymous {3}'.format(self.book, self.rating, self.comments, self.anonymous)
--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,26 @@
-from django.urls import path
-from books import views
+from django.conf.urls import url, include
+from . import views
+from users import urls as users_urls
 
-
+app_name = 'books'
 urlpatterns = [
+    # /books/  this will show a page with all the Books
+    url(r'^$', views.book_list, name='index'),
+    url(r'^search/?$', views.search, name='search_results'),
 
+    # /books/genre/5/ this will show a page with the list of books in genre
+    url(r'^genres/$', views.book_genre, name="books_genres"),
+    url(r'genres/(?P<genre_id>[0-9]+)/$',
+        views.genre_list,
+        name='genre_books'),
+
+    # /books/author/5/ this will show a page with the details of the author
+    url(r'^authors/$', views.AllAuthorsView.as_view(), name="books_authors"),
+    url(r'author/(?P<author_id>[0-9]+)/$',
+        views.author_list,
+        name='author_books'),
+
+    # /books/5/ this will show a page with the details of the book
+    url(r'(?P<book_id>[0-9]+)/$', views.book_detail, name='detail'),
+    url(r'(?P<book_id>[0-9]+)/review$', views.book_review, name='review'),
 ]
